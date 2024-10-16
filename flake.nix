f59b1123f510ae7222ba73f7dc4916075b0e9dff
Joshua Peek <josh@joshpeek.com>
2024-10-16 11:16:32 -0700

    Add macOS package

--- flake.nix
+++ flake.nix
@@ -6,6 +6,7 @@
   };
 
   outputs = { self, nixpkgs }: {
+    packages.aarch64-darwin.hello = nixpkgs.legacyPackages.aarch64-darwin.hello;
     packages.aarch64-linux.hello = nixpkgs.legacyPackages.aarch64-linux.hello;
     packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
   };
