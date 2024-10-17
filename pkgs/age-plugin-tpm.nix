28ebeb4b2a5c82da16ffb2994abc73d172882dc8
Joshua Peek <josh@joshpeek.com>
2024-10-16 19:42:55 -0700

    Add newer age-plugin-tpm

---
+++ pkgs/age-plugin-tpm.nix
@@ -0,0 +1,45 @@
+{
+  lib,
+  age,
+  age-plugin-tpm',
+  age-plugin-tpm,
+  fetchFromGitHub,
+  runCommand,
+}:
+age-plugin-tpm'.overrideAttrs (_oldAttrs: {
+  src = fetchFromGitHub {
+    owner = "Foxboron";
+    repo = "age-plugin-tpm";
+    rev = "5dedd8a6a240ef68851884fe850293e3cc954ac6";
+    hash = "sha256-N6Rsnh5V90SCGO0dd6q5xp7k168PDtX3YNdXF4gLDgI=";
+  };
+  vendorHash = "sha256-qNSQV8GOPdKSzCWUb5ZmXPUb3V6Kgq7w84Tj5zfyLQ4=";
+
+  meta.platforms = lib.platforms.linux ++ lib.platforms.darwin;
+
+  passthru.tests = {
+    help =
+      runCommand "test-age-plugin-tpm-help"
+        {
+          nativeBuildInputs = [ age-plugin-tpm ];
+        }
+        ''
+          age-plugin-tpm --help
+          touch $out
+        '';
+
+    encrypt =
+      runCommand "test-age-plugin-tpm-encrypt"
+        {
+          nativeBuildInputs = [
+            age
+            age-plugin-tpm
+          ];
+        }
+        ''
+          echo "Hello World" | age --encrypt \
+            --recipient "age1tpm1syqqqpqrtxsnkkqlmu505zzrq439hetls4qwwmyhsv8dgjhksvtewvx29lxs7s68qy" \
+            --output "$out"
+        '';
+  };
+})
--- pkgs/default.nix
+++ pkgs/default.nix
@@ -6,6 +6,10 @@ let
   isAvailable = _: pkg: lib.meta.availableOn { inherit system; } pkg;
 
   packages = {
+    age-plugin-tpm = callPackage ./age-plugin-tpm.nix {
+      # TODO: Is there a better way to handle overriding nixpkgs?
+      age-plugin-tpm' = pkgs.age-plugin-tpm;
+    };
     hello = callPackage ./hello.nix { };
     swiftly = callPackage ./swiftly.nix { };
     swift6 = callPackage ./swift.nix { };
