e3c5486412b6d3df836af3f182a534b63bd8b030
Joshua Peek <josh@users.noreply.github.com>
2024-10-17 01:09:23 +0000

    Add swiftly for linux

---
+++ pkgs/swiftly.nix
@@ -0,0 +1,85 @@
+# Upstream to NixOS/nixpkgs
+# - Needs to build from source rather than install binaries.
+#   - Blocked on NixOS/nixpkgs supporting Swift 6.0
+# - Waiting for v0.3.0+ to be released
+#   - Adds `swiftly init`
+#
+{
+  system,
+  lib,
+  stdenv,
+  stdenvNoCC,
+  runCommand,
+  fetchurl,
+  autoPatchelfHook,
+  zlib,
+  swiftly,
+}:
+let
+  version = "0.3.0";
+  sources = {
+    "aarch64-linux" = fetchurl {
+      url = "https://github.com/swiftlang/swiftly/releases/download/0.3.0/swiftly-aarch64-unknown-linux-gnu";
+      sha256 = "0g7xi094h9jk90qa7d9ya1vp16l6rn4m3wrazd42bz5fwirp7z5h";
+    };
+    "x86_64-linux" = fetchurl {
+      url = "https://github.com/swiftlang/swiftly/releases/download/0.3.0/swiftly-x86_64-unknown-linux-gnu";
+      sha256 = "1gll8rq5qrs4wblk8vds9wcfkva0sdmp88kpj2dwvxwjc04x680q";
+    };
+  };
+in
+stdenvNoCC.mkDerivation {
+  pname = "swiftly";
+  inherit version;
+  src = sources.${system};
+  dontUnpack = true;
+  nativeBuildInputs = [
+    autoPatchelfHook
+  ];
+  buildInputs = [
+    stdenv.cc.cc.lib
+    zlib
+  ];
+  installPhase = ''
+    mkdir -p "$out/bin" "$out/share/swiftly"
+    cp "$src" "$out/bin/swiftly"
+    chmod +x "$out/bin/swiftly"
+  '';
+
+  meta = {
+    description = "A Swift toolchain installer and manager, written in Swift";
+    longDescription = ''
+      swiftly is a CLI tool for installing, managing, and switching between Swift toolchains, written in Swift.
+    '';
+    homepage = "https://github.com/swiftlang/swiftly";
+    license = lib.licenses.asl20;
+    sourceProvenance = [ lib.sourceTypes.binaryNativeCode ];
+    mainPackage = "swiftly";
+    platforms = [
+      "aarch64-linux"
+      "x86_64-linux"
+    ];
+  };
+
+  passthru.tests = {
+    version =
+      runCommand "test-swiftly-version"
+        {
+          nativeBuildInputs = [ swiftly ];
+        }
+        ''
+          swiftly --version
+          touch $out
+        '';
+
+    help =
+      runCommand "test-swiftly-help"
+        {
+          nativeBuildInputs = [ swiftly ];
+        }
+        ''
+          swiftly --help
+          touch $out
+        '';
+  };
+}
