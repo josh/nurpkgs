74e29fcdffae9216fc8c9dbf2ec601bce72cbe06
Joshua Peek <josh@users.noreply.github.com>
2024-10-22 00:10:07 +0000

    Build pixlet from source
    Closes #17

--- pkgs/nur/repos/josh/pixlet.nix
+++ pkgs/nur/repos/josh/pixlet.nix
@@ -1,69 +1,66 @@
 # Upstream to NixOS/nixpkgs
-# - Needs to build from source rather than install binaries.
 {
   lib,
-  stdenvNoCC,
-  fetchurl,
+  buildGoModule,
+  fetchFromGitHub,
+  libwebp,
   runCommand,
   testers,
+  nur,
 }:
 let
   version = "0.34.0";
-  sources = {
-    "aarch64-darwin" = fetchurl {
-      url = "https://github.com/tidbyt/pixlet/releases/download/v0.34.0/pixlet_0.34.0_darwin_arm64.tar.gz";
-      sha256 = "0a13wj1pcwv2401zz719inc143n32ffblcax6974gvi32i462dh2";
-    };
-    "aarch64-linux" = fetchurl {
-      url = "https://github.com/tidbyt/pixlet/releases/download/v0.34.0/pixlet_0.34.0_linux_arm64.tar.gz";
-      sha256 = "1bkf4jjqmbwgfhay0yfliqays3pf97vgg53f92hdyphykhbqdhxq";
-    };
-    "x86_64-linux" = fetchurl {
-      url = "https://github.com/tidbyt/pixlet/releases/download/v0.34.0/pixlet_0.34.0_linux_amd64.tar.gz";
-      sha256 = "0h26qpf3ah7v45h4h4v4vycv4rppc8jd19cnwrs67p8331qx0csq";
-    };
-  };
 in
-stdenvNoCC.mkDerivation (finalAttrs: {
+buildGoModule {
   pname = "pixlet";
   inherit version;
 
-  src = sources.${stdenvNoCC.hostPlatform.system};
-  sourceRoot = ".";
+  src = fetchFromGitHub {
+    owner = "tidbyt";
+    repo = "pixlet";
+    rev = "v${version}";
+    hash = "sha256-9OXwXrXqgVYnFIqDz0iSE4o9LmWDIPB7kqX9CVNaQZk=";
+  };
 
-  installPhase = ''
-    mkdir -p "$out/bin"
-    cp pixlet "$out/bin/pixlet"
-  '';
+  vendorHash = "sha256-SbdZylgQJenAAT5kOCPT0mdCs3H/1t1giiiQRB4D0Zo=";
+
+  buildInputs = [ libwebp ];
+
+  CGO_ENABLED = 1;
+
+  ldflags = [
+    "-X 'tidbyt.dev/pixlet/cmd.Version=v${version}'"
+  ];
+
+  # Disable go tests
+  doCheck = false;
 
   meta = {
     description = "Build apps for pixel-based displays";
     homepage = "https://github.com/tidbyt/pixlet";
     license = lib.licenses.asl20;
-    sourceProvenance = [ lib.sourceTypes.binaryNativeCode ];
     mainPackage = "pixlet";
     platforms = [
       "aarch64-darwin"
-      # "aarch64-linux"
+      "aarch64-linux"
       "x86_64-linux"
     ];
   };
 
-  passthru.tests = {
-    version = testers.testVersion {
-      package = finalAttrs.finalPackage;
-      command = "pixlet version";
-      version = "v${finalAttrs.version}";
-    };
+  passthru.tests =
+    let
+      inherit (nur.repos.josh) pixlet;
+    in
+    {
+      version = testers.testVersion {
+        package = pixlet;
+        command = "pixlet version";
+        version = "v${pixlet.version}";
+      };
 
-    help =
-      runCommand "test-pixlet-help"
-        {
-          nativeBuildInputs = [ finalAttrs.finalPackage ];
-        }
-        ''
-          pixlet --help
-          touch $out
-        '';
-  };
-})
+      help = runCommand "test-pixlet-help" { nativeBuildInputs = [ pixlet ]; } ''
+        pixlet --help
+        touch $out
+      '';
+    };
+}
