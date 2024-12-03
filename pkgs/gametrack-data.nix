d6fa50b6912a6b44df0108b96220f5df28d1f705
Joshua Peek <josh@users.noreply.github.com>
2024-12-03 11:19:22 -0800

    Add gametrack-data pkg

---
+++ pkgs/gametrack-data.nix
@@ -0,0 +1,30 @@
+# See <https://github.com/josh/gametrack-data/blob/main/package.nix>
+{
+  lib,
+  fetchFromGitHub,
+  python3Packages,
+}:
+python3Packages.buildPythonApplication rec {
+  pname = "gametrack-data";
+  version = "1.0.0";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "josh";
+    repo = "gametrack-data";
+    rev = "v${version}";
+    hash = "sha256-N6c72fwSLz5KQ82AW/pDQrBbrD55U4WG5EpGcHzMzrY=";
+  };
+
+  build-system = with python3Packages; [
+    hatchling
+  ];
+
+  meta = {
+    description = "Export GameTrack data to CSV";
+    homepage = "https://github.com/josh/gametrack-data";
+    license = lib.licenses.mit;
+    platforms = lib.platforms.darwin;
+    mainProgram = "gametrack-data";
+  };
+}
