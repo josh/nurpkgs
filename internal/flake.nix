56b59dbe79504c77d15c2e1c17f21b24b191fc70
Joshua Peek <josh@users.noreply.github.com>
2024-12-19 12:31:31 -0800

    Work around tracking bug

--- internal/flake.nix
+++ internal/flake.nix
@@ -1,5 +1,7 @@
 {
   inputs = {
+    # https://github.com/renovatebot/renovate/issues/29721
+    # "github:NixOS/nixpkgs/nixpkgs-unstable"
     treefmt-nix.url = "github:numtide/treefmt-nix";
   };
   outputs = _inputs: { };
