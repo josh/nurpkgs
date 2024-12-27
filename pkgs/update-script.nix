4349691bf18f4273b76e83816ba0714633bae659
Joshua Peek <josh@users.noreply.github.com>
2024-12-26 23:03:29 -0800

    Define an internal update maintaince script

---
+++ pkgs/update-script.nix
@@ -0,0 +1,31 @@
+{
+  system,
+  lib,
+  writeShellApplication,
+  nix-update,
+  git,
+  nur,
+}:
+let
+  commands = lib.attrsets.mapAttrsToList (
+    name: pkg:
+    if !(builtins.hasAttr "updateScriptVersion" pkg) then
+      '': skip ${name} does not have updateScriptVersion''
+    else if !pkg.meta.available then
+      '': skip ${name} unavailable on ${system}''
+    else
+      ''nix-update --flake ${name} --version=${pkg.passthru.updateScriptVersion}''
+  ) nur.repos.josh;
+  text = lib.concatStringsSep "\n" commands;
+in
+writeShellApplication {
+  name = "update-script";
+  runtimeEnv = {
+    PATH = lib.strings.makeBinPath [
+      git
+      nix-update
+    ];
+  };
+  bashOptions = [ "xtrace" ];
+  inherit text;
+}
