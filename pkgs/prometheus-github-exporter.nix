077dc634e884115ad55b6d6d1fedf3dcf43e3a40
Joshua Peek <josh@users.noreply.github.com>
2024-12-26 21:14:31 -0800

    Add prometheus-github-exporter package

---
+++ pkgs/prometheus-github-exporter.nix
@@ -0,0 +1,33 @@
+# See <https://github.com/josh/github_exporter/blob/main/package.nix>
+{
+  lib,
+  buildGoModule,
+  fetchFromGitHub,
+}:
+buildGoModule {
+  pname = "prometheus-github-exporter";
+  version = "0.0.0";
+
+  src = fetchFromGitHub {
+    owner = "josh";
+    repo = "github_exporter";
+    rev = "ae12b151b29b6e9c3e68bd919c6c93fb5143ec0e";
+    hash = "sha256-16gUGRGww8HNyhLq02wde8uar/r/kSRJN26BQtwzvbk=";
+  };
+
+  vendorHash = "sha256-SP4tQ+CFmdm+0Ww25IAFW5RL1EzUQsq1/LallwJ1ZT4=";
+
+  env.CGO_ENABLED = 0;
+  ldflags = [
+    "-s"
+    "-w"
+  ];
+
+  meta = {
+    description = "Prometheus exporter for GitHub metrics";
+    homepage = "https://github.com/josh/github_exporter";
+    license = lib.licenses.mit;
+    platforms = lib.platforms.all;
+    mainProgram = "github_exporter";
+  };
+}
