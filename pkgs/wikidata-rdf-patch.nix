adda1feb59b886a8c788f491e7efd09acce6416d
Joshua Peek <josh@users.noreply.github.com>
2024-12-27 22:29:46 -0800

    wikidata-rdf-patch: 0.1.0 -> 0.1.1
    Diff: https://github.com/josh/wikidata-rdf-patch/compare/v0.1.0...v0.1.1

--- pkgs/wikidata-rdf-patch.nix
+++ pkgs/wikidata-rdf-patch.nix
@@ -5,14 +5,14 @@
 }:
 python3Packages.buildPythonApplication rec {
   pname = "wikidata-rdf-patch";
-  version = "0.1.0";
+  version = "0.1.1";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "josh";
     repo = "wikidata-rdf-patch";
     rev = "v${version}";
-    hash = "sha256-a6dlM3Rn078eTELgjVV5hJN4TSizzuVlLJQEinYu+rc=";
+    hash = "sha256-ol3hjD3er6XTuSIRk04DKNb+IU3S0ynOTC6yrTI7jDY=";
   };
 
   build-system = with python3Packages; [
