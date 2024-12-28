f2ccfa1fcfb9f72f3a26c5cc551f54cf8a66fb8a
Joshua Peek <josh@users.noreply.github.com>
2024-12-27 21:01:50 -0800

    Work around version stub

--- pkgs/imdb-plex-sync.nix
+++ pkgs/imdb-plex-sync.nix
@@ -22,7 +22,7 @@ python3Packages.buildPythonApplication rec {
       +++ b/pyproject.toml
       @@ -1,4 +1,5 @@
        [project]
-      +version = "0-unstable-2024-12-23"
+      +version = "0.0.0"
        name = "imdb-plex-sync"
        readme = "README.md"
        authors = [{ name = "Joshua Peek" }]
