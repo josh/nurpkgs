99cd295a1d39bf925f3ae158c0ba39e492930ca3
Joshua Peek <josh@users.noreply.github.com>
2024-12-30 13:41:14 -0800

    Add gh-audit tests
    Closes #48

--- pkgs/gh-audit.nix
+++ pkgs/gh-audit.nix
@@ -2,36 +2,66 @@
   lib,
   fetchFromGitHub,
   python3Packages,
+  runCommand,
+  testers,
 }:
-python3Packages.buildPythonApplication rec {
-  pname = "gh-audit";
-  version = "0.1.1-unstable-2024-12-28";
-  pyproject = true;
+let
+  gh-audit = python3Packages.buildPythonApplication rec {
+    pname = "gh-audit";
+    version = "0.1.1-unstable-2024-12-28";
+    pyproject = true;
 
-  src = fetchFromGitHub {
-    owner = "josh";
-    repo = "gh-audit";
-    rev = "52f4a23541cd7163a3cfbed4e7753b38b5df1fb7";
-    hash = "sha256-+WT7Agg8+NBiJfry43F03o7gcJsQnGVqVwyvyLNHtIE=";
+    src = fetchFromGitHub {
+      owner = "josh";
+      repo = "gh-audit";
+      rev = "52f4a23541cd7163a3cfbed4e7753b38b5df1fb7";
+      hash = "sha256-+WT7Agg8+NBiJfry43F03o7gcJsQnGVqVwyvyLNHtIE=";
+    };
+
+    build-system = with python3Packages; [
+      setuptools
+    ];
+
+    dependencies = with python3Packages; [
+      click
+      pygithub
+      pyyaml
+    ];
+
+    passthru.updateScriptVersion = "branch";
+
+    meta = {
+      description = "Personal GitHub repository meta linting tool for consistent configuration";
+      homepage = "https://github.com/josh/gh-audit";
+      license = lib.licenses.mit;
+      platforms = lib.platforms.all;
+      mainProgram = "gh-audit";
+    };
   };
+in
+gh-audit.overrideAttrs (
+  finalAttrs: _previousAttrs:
+  let
+    gh-audit = finalAttrs.finalPackage;
+    version-parts = lib.versions.splitVersion finalAttrs.version;
+    stable-version = "${builtins.elemAt version-parts 0}.${builtins.elemAt version-parts 1}.${builtins.elemAt version-parts 2}";
+  in
+  {
+    passthru.tests = {
+      version = testers.testVersion {
+        package = gh-audit;
+        version = stable-version;
+      };
 
-  build-system = with python3Packages; [
-    setuptools
-  ];
-
-  dependencies = with python3Packages; [
-    click
-    pygithub
-    pyyaml
-  ];
-
-  passthru.updateScriptVersion = "branch";
-
-  meta = {
-    description = "Personal GitHub repository meta linting tool for consistent configuration";
-    homepage = "https://github.com/josh/gh-audit";
-    license = lib.licenses.mit;
-    platforms = lib.platforms.all;
-    mainProgram = "gh-audit";
-  };
-}
+      help =
+        runCommand "test-gh-audit-help"
+          {
+            nativeBuildInputs = [ gh-audit ];
+          }
+          ''
+            gh-audit --help
+            touch $out
+          '';
+    };
+  }
+)
