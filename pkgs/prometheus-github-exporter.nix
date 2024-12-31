35d58b75e9f9f64a0fbfe7c7423105d80636df47
Joshua Peek <josh@users.noreply.github.com>
2024-12-31 15:42:59 -0800

    Add tests

--- pkgs/prometheus-github-exporter.nix
+++ pkgs/prometheus-github-exporter.nix
@@ -1,37 +1,66 @@
-# See <https://github.com/josh/github_exporter/blob/main/package.nix>
 {
   lib,
   buildGoModule,
   fetchFromGitHub,
   nix-update-script,
+  runCommand,
+  testers,
 }:
-buildGoModule {
-  pname = "prometheus-github-exporter";
-  version = "0.1.0-unstable-2024-12-31";
+let
+  prometheus-github-exporter = buildGoModule {
+    pname = "prometheus-github-exporter";
+    version = "0.1.0-unstable-2024-12-31";
 
-  src = fetchFromGitHub {
-    owner = "josh";
-    repo = "github_exporter";
-    rev = "e0652108a81b26db209007c08638a848a8f932c6";
-    hash = "sha256-+DsMdqOJPSbjLki+23fROcog2rDYn5jsPnOcEfckcys=";
+    src = fetchFromGitHub {
+      owner = "josh";
+      repo = "github_exporter";
+      rev = "e0652108a81b26db209007c08638a848a8f932c6";
+      hash = "sha256-+DsMdqOJPSbjLki+23fROcog2rDYn5jsPnOcEfckcys=";
+    };
+
+    vendorHash = "sha256-TByHG/Fk20Bh9qI+tTwBFm47A0kb+SCc7hQ8s9RkwKk=";
+
+    env.CGO_ENABLED = 0;
+    ldflags = [
+      "-s"
+      "-w"
+    ];
+
+    meta = {
+      description = "Prometheus exporter for GitHub metrics";
+      homepage = "https://github.com/josh/github_exporter";
+      license = lib.licenses.mit;
+      platforms = lib.platforms.all;
+      mainProgram = "github_exporter";
+    };
   };
+in
+prometheus-github-exporter.overrideAttrs (
+  finalAttrs: _previousAttrs:
+  let
+    prometheus-github-exporter = finalAttrs.finalPackage;
+    version-parts = lib.versions.splitVersion finalAttrs.version;
+    stable-version = "${builtins.elemAt version-parts 0}.${builtins.elemAt version-parts 1}.${builtins.elemAt version-parts 2}";
+  in
+  {
+    passthru.updateScriptVersion = "branch";
+    passthru.updateScript = nix-update-script { extraArgs = [ "--version=branch" ]; };
 
-  vendorHash = "sha256-TByHG/Fk20Bh9qI+tTwBFm47A0kb+SCc7hQ8s9RkwKk=";
+    passthru.tests = {
+      version = testers.testVersion {
+        package = prometheus-github-exporter;
+        version = stable-version;
+      };
 
-  env.CGO_ENABLED = 0;
-  ldflags = [
-    "-s"
-    "-w"
-  ];
-
-  passthru.updateScriptVersion = "branch";
-  passthru.updateScript = nix-update-script { extraArgs = [ "--version=branch" ]; };
-
-  meta = {
-    description = "Prometheus exporter for GitHub metrics";
-    homepage = "https://github.com/josh/github_exporter";
-    license = lib.licenses.mit;
-    platforms = lib.platforms.all;
-    mainProgram = "github_exporter";
-  };
-}
+      help =
+        runCommand "test-prometheus-github-exporter-help"
+          {
+            nativeBuildInputs = [ prometheus-github-exporter ];
+          }
+          ''
+            github_exporter --help
+            touch $out
+          '';
+    };
+  }
+)
