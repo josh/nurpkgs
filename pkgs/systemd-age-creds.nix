186fe7c4d802b7e164d620fc169ef743b8f8c388
Joshua Peek <josh@users.noreply.github.com>
2024-12-31 16:36:45 -0800

    Add help and version test

--- pkgs/systemd-age-creds.nix
+++ pkgs/systemd-age-creds.nix
@@ -5,37 +5,67 @@
   fetchFromGitHub,
   age,
   nix-update-script,
+  runCommand,
+  testers,
 }:
-buildGoModule rec {
-  pname = "systemd-age-creds";
-  version = "0.1.0-unstable-2025-01-01";
+let
+  systemd-age-creds = buildGoModule rec {
+    pname = "systemd-age-creds";
+    version = "0.1.0-unstable-2025-01-01";
 
-  src = fetchFromGitHub {
-    owner = "josh";
-    repo = "systemd-age-creds";
-    rev = "241e5bf8df057a54a21afe7484f0bd70eff33801";
-    hash = "sha256-IOXMPo97w6wZ2eMH8NdmsBn6ZClueSx/cjny1F1Iw8E=";
+    src = fetchFromGitHub {
+      owner = "josh";
+      repo = "systemd-age-creds";
+      rev = "241e5bf8df057a54a21afe7484f0bd70eff33801";
+      hash = "sha256-IOXMPo97w6wZ2eMH8NdmsBn6ZClueSx/cjny1F1Iw8E=";
+    };
+    vendorHash = null;
+
+    env.CGO_ENABLED = 0;
+    ldflags = [
+      "-s"
+      "-w"
+      "-X main.Version=${version}"
+      "-X main.AgeBin=${lib.getExe age}"
+    ];
+
+    nativeBuildInputs = [ age ];
+
+    passthru.updateScriptVersion = "branch";
+    passthru.updateScript = nix-update-script { extraArgs = [ "--version=branch" ]; };
+
+    meta = {
+      description = "Load age encrypted credentials in systemd units";
+      mainProgram = "systemd-age-creds";
+      homepage = "https://github.com/josh/systemd-age-creds";
+      license = lib.licenses.mit;
+      platforms = lib.platforms.linux;
+    };
   };
-  vendorHash = null;
+in
+systemd-age-creds.overrideAttrs (
+  finalAttrs: _previousAttrs:
+  let
+    systemd-age-creds = finalAttrs.finalPackage;
+    version-parts = lib.versions.splitVersion finalAttrs.version;
+    stable-version = "${builtins.elemAt version-parts 0}.${builtins.elemAt version-parts 1}.${builtins.elemAt version-parts 2}";
+  in
+  {
+    passthru.tests = {
+      version = testers.testVersion {
+        package = systemd-age-creds;
+        version = stable-version;
+      };
 
-  env.CGO_ENABLED = 0;
-  ldflags = [
-    "-s"
-    "-w"
-    "-X main.Version=${version}"
-    "-X main.AgeBin=${lib.getExe age}"
-  ];
-
-  nativeBuildInputs = [ age ];
-
-  passthru.updateScriptVersion = "branch";
-  passthru.updateScript = nix-update-script { extraArgs = [ "--version=branch" ]; };
-
-  meta = {
-    description = "Load age encrypted credentials in systemd units";
-    mainProgram = "systemd-age-creds";
-    homepage = "https://github.com/josh/systemd-age-creds";
-    license = lib.licenses.mit;
-    platforms = lib.platforms.linux;
-  };
-}
+      help =
+        runCommand "test-systemd-age-creds-help"
+          {
+            nativeBuildInputs = [ systemd-age-creds ];
+          }
+          ''
+            systemd-age-creds --help
+            touch $out
+          '';
+    };
+  }
+)
