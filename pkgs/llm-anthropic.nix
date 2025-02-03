58b0563147187a6055cb2ea7f85e1a3b7fbd1932
Joshua Peek <josh@users.noreply.github.com>
2025-02-02 21:11:00 -0800

    Add llm-anthropic

---
+++ pkgs/llm-anthropic.nix
@@ -0,0 +1,40 @@
+{
+  lib,
+  fetchFromGitHub,
+  python3Packages,
+  nur,
+}:
+python3Packages.buildPythonPackage rec {
+  pname = "llm-anthropic";
+  version = "0.12";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "simonw";
+    repo = "llm-anthropic";
+    tag = version;
+    hash = "sha256-7+5j5jZBFfaaqnfjvLTI+mz1PUuG8sB5nD59UCpJuR4=";
+  };
+
+  build-system = [
+    python3Packages.setuptools
+    python3Packages.llm
+  ];
+
+  dependencies = [
+    nur.repos.josh.anthropic
+  ];
+
+  pythonImportsCheck = [
+    "llm_anthropic"
+  ];
+
+  passthru.updateScriptVersion = "stable";
+
+  meta = {
+    description = "LLM access to models by Anthropic, including the Claude series";
+    homepage = "https://github.com/simonw/llm-anthropic";
+    changelog = "https://github.com/simonw/llm-anthropic/releases/tag/${version}";
+    license = lib.licenses.asl20;
+  };
+}
