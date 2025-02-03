66df8289588d8841ba6ff976a906c52200a8f36d
Joshua Peek <josh@MaxBook-Pro.local>
2025-02-02 20:52:53 -0800

    anthropic: 0.42.0 -> 0.45.2
    Diff: https://github.com/anthropics/anthropic-sdk-python/compare/refs/tags/v0.42.0...v0.45.2
    
    Changelog: https://github.com/anthropics/anthropic-sdk-python/releases/tag/v0.42.0

--- pkgs/anthropic.nix
+++ pkgs/anthropic.nix
@@ -1,11 +1,11 @@
 { fetchFromGitHub, python3Packages }:
 python3Packages.anthropic.overrideAttrs rec {
-  version = "0.42.0";
+  version = "0.45.2";
   src = fetchFromGitHub {
     owner = "anthropics";
     repo = "anthropic-sdk-python";
     tag = "v${version}";
-    hash = "sha256-7cRXKXiyq3ty21klkitjjnm9rzBRmAXGYvvVxTNWeZ4=";
+    hash = "sha256-/QuAUU0/nNhJZouCP0LVkCFMTiNdeze/fF+SZKD1Jis=";
   };
 
   passthru.updateScriptVersion = "stable";
