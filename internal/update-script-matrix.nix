df5b6d171ba11760a77501b3a2c642acd38f95df
Joshua Peek <josh@users.noreply.github.com>
2025-02-13 10:37:40 -0800

    Split up update into matrix
    Related #50

---
+++ internal/update-script-matrix.nix
@@ -0,0 +1,31 @@
+# FLAKE_URI="$PWD" nix eval --raw --file ./internal/update-script-matrix.nix
+let
+  system = builtins.currentSystem;
+  flake = builtins.getFlake (builtins.getEnv "FLAKE_URI");
+  pkgs = flake.packages.${system};
+
+  commands = builtins.filter (x: x != null) (
+    builtins.map (
+      name:
+      let
+        pkg = pkgs.${name};
+      in
+      if !(builtins.hasAttr "updateScriptVersion" pkg) then
+        null
+      else if !pkg.meta.available then
+        null
+      else
+        {
+          "attr" = name;
+          "version" = pkg.passthru.updateScriptVersion;
+        }
+    ) (builtins.attrNames pkgs)
+  );
+
+  matrix = {
+    include = commands;
+  };
+in
+"matrix=${builtins.toJSON matrix}"
+
+# --write-to
