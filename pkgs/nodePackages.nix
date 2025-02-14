30b0bad038fd1e054725d3d494c54648bdb4b134
Joshua Peek <josh@users.noreply.github.com>
2025-02-14 12:54:38 -0800

    Inline file

--- pkgs/nodePackages.nix
+++ pkgs/nodePackages.nix
@@ -1,18 +1,32 @@
 {
   lib,
+  writers,
   callPackage,
   writeShellScript,
+  nix,
   node2nix,
 }:
 let
   nodeEnv = callPackage ./nodePackages/node-env.nix { };
 
+  packageNames = [
+    "@modelcontextprotocol/inspector"
+    "@modelcontextprotocol/server-filesystem"
+  ];
+  packageNamesJSON = writers.writeJSON "node-packages.json" packageNames;
+
   updateScript = writeShellScript "update-node-packages" ''
     set -o errexit
     set -o xtrace
-    export PATH=${lib.strings.makeBinPath [ node2nix ]}
+    export PATH=${
+      lib.strings.makeBinPath [
+        nix
+        node2nix
+      ]
+    }
     cd pkgs/nodePackages
-    node2nix --input ./node-packages.json --composition /dev/null
+    node2nix --input ${packageNamesJSON} --composition /dev/null
+    nix fmt
   '';
 
   pkgs = callPackage ./nodePackages/node-packages.nix {
