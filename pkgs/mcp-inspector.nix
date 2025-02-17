ea6374bdf9976ab4b0469ba4830424c72031812c
Joshua Peek <josh@users.noreply.github.com>
2025-02-16 17:15:45 -0800

    Improve some pkg final names

--- pkgs/mcp-inspector.nix
+++ pkgs/mcp-inspector.nix
@@ -1,6 +1,8 @@
 { nur, runCommand }:
 nur.repos.josh.nodePackages."@modelcontextprotocol/inspector".overrideAttrs (
-  _finalAttrs: _previousAttrs: {
+  finalAttrs: _previousAttrs: {
+    name = "mcp-inspector-${finalAttrs.version}";
+    pname = "mcp-inspector";
     meta.mainProgram = "mcp-inspector";
   }
 )
--- pkgs/mcp-server-filesystem.nix
+++ pkgs/mcp-server-filesystem.nix
@@ -1,6 +1,8 @@
 { nur, runCommand }:
 nur.repos.josh.nodePackages."@modelcontextprotocol/server-filesystem".overrideAttrs (
-  _finalAttrs: _previousAttrs: {
+  finalAttrs: _previousAttrs: {
+    name = "mcp-server-filesystem-${finalAttrs.version}";
+    pname = "mcp-server-filesystem";
     meta.mainProgram = "mcp-server-filesystem";
   }
 )
