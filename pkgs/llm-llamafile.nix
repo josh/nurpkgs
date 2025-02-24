4965ea91e5c6f02cc0f613d4109e236b5424ff7e
Joshua Peek <josh@users.noreply.github.com>
2025-02-24 13:49:03 -0800

    Add llm-llamafile
    Related #129

---
+++ pkgs/llm-llamafile.nix
@@ -0,0 +1,39 @@
+{
+  lib,
+  fetchFromGitHub,
+  python3Packages,
+}:
+python3Packages.buildPythonPackage rec {
+  __structuredAttrs = true;
+
+  pname = "llm-llamafile";
+  version = "0.1";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "simonw";
+    repo = "llm-llamafile";
+    tag = version;
+    hash = "sha256-LF1LhKgi7Pm1cRpFweyawDqXiDwfexa37HtpgOs6JoQ=";
+  };
+
+  build-system = [
+    python3Packages.setuptools
+    python3Packages.llm
+  ];
+
+  dependencies = [ ];
+
+  pythonImportsCheck = [
+    "llm_llamafile"
+  ];
+
+  passthru.updateScriptVersion = "stable";
+
+  meta = {
+    description = "Access llamafile localhost models via LLM";
+    homepage = "https://github.com/simonw/llm-llamafile";
+    changelog = "https://github.com/simonw/llm-llamafile/releases/tag/${version}";
+    license = lib.licenses.asl20;
+  };
+}
