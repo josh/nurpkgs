4ef36ece92c09d6a86df5ff03437d62b59c5bdd3
Joshua Peek <josh@users.noreply.github.com>
2025-02-24 13:53:59 -0800

    Add llm-sentence-transformers package
    Related #129

---
+++ pkgs/llm-sentence-transformers.nix
@@ -0,0 +1,41 @@
+{
+  lib,
+  fetchFromGitHub,
+  python3Packages,
+}:
+python3Packages.buildPythonPackage rec {
+  __structuredAttrs = true;
+
+  pname = "llm-sentence-transformers";
+  version = "0.3";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "simonw";
+    repo = "llm-sentence-transformers";
+    tag = version;
+    hash = "sha256-FM0ZI5oJCOEruS+EtiUxEvvldynFLSu0vOZHcVgaQws=";
+  };
+
+  build-system = [
+    python3Packages.setuptools
+    python3Packages.llm
+  ];
+
+  dependencies = [
+    python3Packages.sentence-transformers
+  ];
+
+  pythonImportsCheck = [
+    "llm_sentence_transformers"
+  ];
+
+  passthru.updateScriptVersion = "stable";
+
+  meta = {
+    description = "LLM plugin for embeddings using sentence-transformers";
+    homepage = "https://github.com/simonw/llm-sentence-transformers";
+    changelog = "https://github.com/simonw/llm-sentence-transformers/releases/tag/${version}";
+    license = lib.licenses.asl20;
+  };
+}
