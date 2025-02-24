fa952c2b25a0d377edff5e50befd3a8451fe1351
Joshua Peek <josh@users.noreply.github.com>
2025-02-23 19:45:45 -0800

    Add testscript package

---
+++ pkgs/testscript.nix
@@ -0,0 +1,34 @@
+{
+  lib,
+  buildGoModule,
+  fetchFromGitHub,
+}:
+buildGoModule {
+  pname = "testscript";
+  version = "1.13.1";
+
+  src = fetchFromGitHub {
+    owner = "rogpeppe";
+    repo = "go-internal";
+    tag = "v1.13.1";
+    hash = "sha256-fD4n3XVDNHL7hfUXK9qi31LpBVzWnRK/7LNc3BmPtnU=";
+  };
+
+  vendorHash = "sha256-rX2xcv409FoC9vO65iLj7w+S8ykBG+6aE3ooD0h1Abg=";
+
+  subPackages = [
+    "cmd/testscript"
+  ];
+
+  doCheck = false;
+
+  meta = {
+    description = "Provides a shell-like test environment that is very nicely tuned for testing Go CLI commands";
+    homepage = "https://github.com/rogpeppe/go-internal";
+    license = lib.licenses.bsd3;
+    platforms = lib.platforms.all;
+    mainProgram = "testscript";
+  };
+
+  passthru.updateScriptVersion = "stable";
+}
