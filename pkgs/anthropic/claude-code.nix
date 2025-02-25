1aedd55f3f369eb671e335540e0f6b826733d64e
Joshua Peek <josh@users.noreply.github.com>
2025-02-25 14:56:36 -0800

    Remove claude-code pkg
    See upstream `nixpkgs#claude-code`

--- pkgs/anthropic/claude-code.nix
+++
@@ -1,9 +0,0 @@
-{ nur, runCommand }:
-nur.repos.josh.nodePackages."@anthropic-ai/claude-code".overrideAttrs (
-  finalAttrs: _previousAttrs: {
-    pname = "claude-code";
-    name = "claude-code-${finalAttrs.version}";
-
-    meta.mainProgram = "claude";
-  }
-)
