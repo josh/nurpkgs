db9f378dcdc7b1b283636eb6f17b5f0fce0abeba
Joshua Peek <josh@users.noreply.github.com>
2025-03-07 20:51:47 -0800

    Add unstable xdg-ninja

---
+++ pkgs/b3nj5m1n/xdg-ninja.nix
@@ -0,0 +1,19 @@
+{
+  xdg-ninja,
+  fetchFromGitHub,
+  nix-update-script,
+}:
+xdg-ninja.overrideAttrs (
+  _finalAttrs: _previousAttrs: {
+    version = "0.1.0";
+
+    src = fetchFromGitHub {
+      owner = "b3nj5m1n";
+      repo = "xdg-ninja";
+      rev = "8a3d7751d18a64dcab9245f03b9cb0829ca08471";
+      hash = "sha256-dHDNSQOnkO/sRvHZz13dhf8ghhZ+gcN2aQTQXRH8+P8=";
+    };
+
+    passthru.updateScript = nix-update-script { extraArgs = [ "--version=branch" ]; };
+  }
+)
