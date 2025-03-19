818285bbb2589159da2f771b8f1675609c16b236
Joshua Peek <josh@users.noreply.github.com>
2025-03-18 19:30:50 -0700

    Add trakt-data package

---
+++ pkgs/josh/trakt-data.nix
@@ -0,0 +1,64 @@
+{
+  lib,
+  fetchFromGitHub,
+  python3Packages,
+  runCommand,
+  nix-update-script,
+}:
+let
+  trakt-data = python3Packages.buildPythonApplication rec {
+    pname = "trakt-data";
+    version = "0.0.0";
+
+    src = fetchFromGitHub {
+      owner = "josh";
+      repo = "trakt-data";
+      rev = "9d9e0d6719c61aa1957fd65a81397fd62ddc4324";
+      hash = "sha256-xd1tcy84c6OMCW8uTzOhd91VC4SPiYxweSbOPrvvR84=";
+    };
+
+    pyproject = true;
+    __structuredAttrs = true;
+
+    build-system = with python3Packages; [
+      hatchling
+    ];
+
+    dependencies = with python3Packages; [
+      click
+      requests
+    ];
+
+    meta = {
+      description = "Export Trakt data";
+      homepage = "https://github.com/josh/trakt-data";
+      license = lib.licenses.mit;
+      platforms = lib.platforms.all;
+      mainProgram = "trakt-data";
+    };
+  };
+in
+trakt-data.overrideAttrs (
+  finalAttrs: _previousAttrs:
+  let
+    trakt-data = finalAttrs.finalPackage;
+  in
+  {
+    passthru.updateScript = nix-update-script { extraArgs = [ "--version=branch" ]; };
+
+    passthru.tests = {
+      # TODO: Add --version test
+
+      help =
+        runCommand "test-trakt-data-help"
+          {
+            __structuredAttrs = true;
+            nativeBuildInputs = [ trakt-data ];
+          }
+          ''
+            trakt-data --help
+            touch $out
+          '';
+    };
+  }
+)
