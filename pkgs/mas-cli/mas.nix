5552cbaab275d9928c992f2e936f0d3c3f67e04c
Joshua Peek <josh@users.noreply.github.com>
2025-04-19 13:32:25 -0700

    Upgrade to mas 2.1.0

---
+++ pkgs/mas-cli/mas.nix
@@ -0,0 +1,10 @@
+{ fetchurl, mas }:
+mas.overrideAttrs (
+  finalAttrs: previousAttrs: {
+    version = "2.1.0";
+    src = fetchurl {
+      url = "https://github.com/mas-cli/mas/releases/download/v${finalAttrs.version}/mas-${finalAttrs.version}.pkg";
+      hash = "sha256-pT8W/ZdNP7Fv5nyTX9vKbTa2jIk3THN1HVCmuEIibfc=";
+    };
+  }
+)
