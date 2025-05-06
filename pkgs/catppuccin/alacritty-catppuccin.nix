0a98422e1fa5a02338cd0f058b7f58a507e303ec
Joshua Peek <josh@users.noreply.github.com>
2025-05-06 11:44:25 -0700

    Revert "Upstreamed alacritty catppuccin package"
    This reverts commit 7dfb26b82ee72a56edf7ad058b7165de4d7db25a.

---
+++ pkgs/catppuccin/alacritty-catppuccin.nix
@@ -0,0 +1,31 @@
+{
+  lib,
+  stdenvNoCC,
+  fetchFromGitHub,
+  nix-update-script,
+}:
+stdenvNoCC.mkDerivation {
+  pname = "alacritty-catppuccin";
+  version = "yaml-unstable-2024-10-28";
+
+  src = fetchFromGitHub {
+    owner = "catppuccin";
+    repo = "alacritty";
+    rev = "f6cb5a5c2b404cdaceaff193b9c52317f62c62f7";
+    hash = "sha256-H8bouVCS46h0DgQ+oYY8JitahQDj0V9p2cOoD4cQX+Q=";
+  };
+
+  buildCommand = ''
+    mkdir $out
+    cp $src/*.toml $out/
+  '';
+
+  passthru.updateScript = nix-update-script { extraArgs = [ "--version=branch" ]; };
+
+  meta = {
+    description = "Soothing pastel theme for Alacritty";
+    homepage = "https://github.com/catppuccin/alacritty";
+    license = lib.licenses.mit;
+    platforms = lib.platforms.all;
+  };
+}
