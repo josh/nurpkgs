8bbcd5e02d33ca4fe41adec26dd5b4b957a821a6
Joshua Peek <josh@users.noreply.github.com>
2025-06-24 10:34:28 -0700

    Drop mcp server packages

--- pkgs/modelcontextprotocol/mcp-server-memory.nix
+++
@@ -1,9 +0,0 @@
-{ nur, runCommand }:
-nur.repos.josh.nodePackages."@modelcontextprotocol/server-memory".overrideAttrs (
-  finalAttrs: _previousAttrs: {
-    pname = "mcp-server-memory";
-    name = "mcp-server-memory-${finalAttrs.version}";
-
-    meta.mainProgram = "mcp-server-memory";
-  }
-)
