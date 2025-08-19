ab9be134c8f6ddca8977c6e8deaf806f63e444fa
Joshua Peek <josh@users.noreply.github.com>
2025-08-19 16:20:28 -0700

    Add plex-index package

---
+++ pkgs/josh/plex-index.nix
@@ -0,0 +1,66 @@
+{
+  lib,
+  fetchFromGitHub,
+  python3Packages,
+  runCommand,
+  nix-update-script,
+}:
+let
+  plex-index = python3Packages.buildPythonApplication {
+    pname = "plex-index";
+    version = "0.0.0";
+
+    src = fetchFromGitHub {
+      owner = "josh";
+      repo = "plex-index";
+      rev = "f8f7bee02aa883341634891c3981b8b6c6fa960a";
+      hash = "sha256-XtPALL17W672D7QPVH+S1XvJqzhLpoZZ6NBnAsCrakI=";
+    };
+
+    pyproject = true;
+
+    build-system = with python3Packages; [
+      hatchling
+    ];
+
+    dependencies = with python3Packages; [
+      click
+      polars
+      tqdm
+    ];
+
+    meta = {
+      description = "Compact Plex external ID index";
+      homepage = "https://github.com/josh/plex-index";
+      license = lib.licenses.mit;
+      platforms = lib.platforms.all;
+      mainProgram = "plex-index";
+      # FIXME: Broken on nixos-25.05
+      broken = python3Packages.polars.version == "1.27.1";
+    };
+  };
+in
+plex-index.overrideAttrs (
+  finalAttrs: previousAttrs:
+  let
+    plex-index = finalAttrs.finalPackage;
+  in
+  {
+    passthru = previousAttrs.passthru // {
+      updateScript = nix-update-script { extraArgs = [ "--version=branch" ]; };
+
+      tests = {
+        help =
+          runCommand "test-plex-index-help"
+            {
+              __structuredAttrs = true;
+              nativeBuildInputs = [ plex-index ];
+            }
+            ''
+              plex-index --help
+              touch $out
+            '';
+      };
+    };
+  }
+)
