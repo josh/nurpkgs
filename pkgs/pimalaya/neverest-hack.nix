69701923d50a2a3363bb64c70efa256c332ff605
Joshua Peek <josh@users.noreply.github.com>
2025-08-25 12:43:09 -0700

    Add neverest-hack thats pinned to old rust-c

---
+++ pkgs/pimalaya/neverest-hack.nix
@@ -0,0 +1,9 @@
+{ system }:
+let
+  nixpkgs = fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/f937f8ecd1c70efd7e9f90ba13dfb400cf559de4.tar.gz";
+    sha256 = "sha256-4/Jd+LkQ2ssw8luQVkqVs9spDBVE6h/u/hC/tzngsPo=";
+  };
+  pkgs = import nixpkgs { inherit system; };
+in
+pkgs.callPackage ./neverest.nix { }
