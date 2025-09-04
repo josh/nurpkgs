6025143342d40fcf8c4ac717343296a6037ebd06
Joshua Peek <josh@users.noreply.github.com>
2025-09-04 12:24:30 -0700

    Set position by hand

--- pkgs/cased/terraform-provider-github.nix
+++ pkgs/cased/terraform-provider-github.nix
@@ -1,18 +1,22 @@
 { terraform-providers, nix-update-script }:
-(terraform-providers.mkProvider {
-  owner = "cased";
-  repo = "terraform-provider-github";
-  rev = "9826ea55be7b673c0f3d8513f3ba5ae097b91916";
-  hash = "sha256-ITnpR4JMWrkFjfxEK62TgujXHVL7kiA8J5ziytAeTAQ=";
-  vendorHash = null;
-  provider-source-address = "registry.terraform.io/cased/github";
-  homepage = "https://github.com/cased/terraform-provider-github";
-  spdx = "MIT";
-}).overrideAttrs
-  (
-    _finalAttrs: previousAttrs: {
-      passthru = previousAttrs.passthru // {
-        updateScript = nix-update-script { extraArgs = [ "--version=branch" ]; };
-      };
-    }
-  )
+let
+  pkg = terraform-providers.mkProvider {
+      owner = "cased";
+      repo = "terraform-provider-github";
+      rev = "9826ea55be7b673c0f3d8513f3ba5ae097b91916";
+      hash = "sha256-ITnpR4JMWrkFjfxEK62TgujXHVL7kiA8J5ziytAeTAQ=";
+      vendorHash = null;
+      provider-source-address = "registry.terraform.io/cased/github";
+      homepage = "https://github.com/cased/terraform-provider-github";
+      spdx = "MIT";
+    };
+in
+pkg
+// {
+  passthru = pkg.passthru // {
+    updateScript = nix-update-script { extraArgs = [ "--version=branch" ]; };
+  };
+  meta = pkg.meta // {
+    position = "${./terraform-provider-github.nix}:17";
+  };
+}
