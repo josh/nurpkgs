028da18dcc311cb5b673c22f08f1095000d43fd0
Joshua Peek <josh@users.noreply.github.com>
2025-09-10 12:46:02 -0700

    Add bugjour package

---
+++ pkgs/josh/bugjour.nix
@@ -0,0 +1,35 @@
+{
+  lib,
+  buildGoModule,
+  fetchFromGitHub,
+  nix-update-script,
+}:
+buildGoModule {
+  pname = "bugjour";
+  version = "0.0.0";
+
+  src = fetchFromGitHub {
+    owner = "josh";
+    repo = "bugjour";
+    rev = "64d284c95e20c3fe98160086ff1b06c5051d8e45";
+    hash = "sha256-1uu7N9KyvEcFYsmjDcau0JlAWi6XhQxHNnoH+U8oyFs=";
+  };
+
+  vendorHash = "sha256-4V3cIgEN8WkHHrPz9SRshoiu0C+NHR0Xov1FZ06Q9XI=";
+
+  env.CGO_ENABLED = 0;
+  ldflags = [
+    "-s"
+    "-w"
+  ];
+
+  passthru.updateScript = nix-update-script { extraArgs = [ "--version=branch" ]; };
+
+  meta = {
+    description = "Detecting Apple Bonjour hostname conflicts…";
+    homepage = "https://github.com/josh/bugjour";
+    license = lib.licenses.mit;
+    platforms = lib.platforms.all;
+    mainProgram = "bugjour";
+  };
+}
