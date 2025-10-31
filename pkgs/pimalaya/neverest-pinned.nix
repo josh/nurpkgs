425b28a0c07699f45fde145cc079173c5f34475e
Joshua Peek <josh@users.noreply.github.com>
2025-10-31 11:02:55 -0700

    'system' has been renamed to/replaced by 'stdenv.hostPlatform.system'

--- pkgs/pimalaya/neverest-pinned.nix
+++ pkgs/pimalaya/neverest-pinned.nix
@@ -1,10 +1,10 @@
-{ system }:
+{ stdenv }:
 let
   nixpkgs = fetchTarball {
     url = "https://github.com/NixOS/nixpkgs/archive/f937f8ecd1c70efd7e9f90ba13dfb400cf559de4.tar.gz";
     sha256 = "sha256-4/Jd+LkQ2ssw8luQVkqVs9spDBVE6h/u/hC/tzngsPo=";
   };
-  pkgs = import nixpkgs { inherit system; };
+  pkgs = import nixpkgs { inherit (stdenv.hostPlatform) system; };
 in
 (pkgs.callPackage ./neverest.nix { }).overrideAttrs (previousAttrs: {
   meta = previousAttrs.meta // {
