5c8e241720933a9ecb5cc5f80411c9f699e55cd9
Joshua Peek <josh@users.noreply.github.com>
2025-12-15 23:53:50 -0800

    Add tailscale operator charts

---
+++ pkgs/tailscale/tailscale-operator-charts-update-script.nix
@@ -0,0 +1,33 @@
+{
+  writeShellApplication,
+  coreutils,
+  curl,
+  git,
+  gnused,
+  nix,
+  yq,
+}:
+writeShellApplication {
+  name = "tailscale-operator-charts-update-script";
+  runtimeInputs = [
+    coreutils
+    curl
+    git
+    gnused
+    nix
+    yq
+  ];
+  text = ''
+    index_path=$(mktemp --suffix=.yaml)
+    curl --silent --show-error --fail https://pkgs.tailscale.com/helmcharts/index.yaml --output "$index_path"
+    version=$(yq --raw-output '.entries["tailscale-operator"][0].version' "$index_path")
+    url=$(yq --raw-output '.entries["tailscale-operator"][0] | "https://pkgs.tailscale.com/helmcharts/\(.urls[0])"' "$index_path")
+    sha256=$(nix-prefetch-url --unpack "$url")
+    filename=pkgs/tailscale/tailscale-operator-charts.nix
+    sed -i "s|version = \".*\";|version = \"$version\";|" "$filename"
+    sed -i "s|url = \".*\";|url = \"$url\";|" "$filename"
+    sed -i "s|sha256 = \".*\";|sha256 = \"$sha256\";|" "$filename"
+    git add "$filename"
+    git commit --message "tailscale-operator-charts: $version"
+  '';
+}
---
+++ pkgs/tailscale/tailscale-operator-charts.nix
@@ -0,0 +1,49 @@
+{
+  lib,
+  stdenvNoCC,
+  fetchzip,
+  kubernetes-helm,
+  yq,
+  nur,
+}:
+stdenvNoCC.mkDerivation {
+  pname = "tailscale-operator-charts";
+  version = "1.90.6";
+
+  src = fetchzip {
+    url = "https://pkgs.tailscale.com/helmcharts/tailscale-operator-1.90.6-1761949884-08247dd90325a32ae95c5b116917015458b569eadbd66e7116dcdc7502a82bd9.tgz";
+    sha256 = "sha256-cHlHdq+YufyW1YNfFZ3O7U8FbGxZIbg5ryYp6eExReg=";
+  };
+
+  __structuredAttrs = true;
+
+  nativeBuildInputs = [
+    kubernetes-helm
+    yq
+  ];
+
+  helmArgs = [ ];
+  helmValues = { };
+
+  buildPhase = ''
+    runHook preBuild
+    yq --yaml-output '.helmValues' "$NIX_ATTRS_JSON_FILE" >values.yaml
+    helm template tailscale-operator "$src" --output-dir . --values values.yaml "''${helmArgs[@]}"
+    runHook postBuild
+  '';
+
+  installPhase = ''
+    runHook preInstall
+    mkdir -p $out
+    cp -R ./tailscale-operator/templates/* $out
+    runHook postInstall
+  '';
+
+  passthru.updateScript = [ "${lib.getExe nur.repos.josh.tailscale-operator-charts-update-script}" ];
+
+  meta = {
+    description = "A Helm chart for Tailscale Kubernetes operator";
+    homepage = "https://github.com/tailscale/tailscale/tree/main/cmd/k8s-operator/deploy/chart";
+    license = lib.licenses.bsd3;
+  };
+}
