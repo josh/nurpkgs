9f8cd1241ef233419049b7c14d6a3cd2587f7730
Joshua Peek <josh@users.noreply.github.com>
2025-12-27 14:41:09 -0800

    Add nats helm chart

---
+++ pkgs/nats-io/nats-charts.nix
@@ -0,0 +1,55 @@
+{
+  lib,
+  stdenvNoCC,
+  fetchzip,
+  kubernetes-helm,
+  yq,
+  nur,
+}:
+stdenvNoCC.mkDerivation {
+  pname = "nats-charts";
+  version = "2.12.3";
+
+  src = fetchzip {
+    url = "https://github.com/nats-io/k8s/releases/download/nats-2.12.3/nats-2.12.3.tgz";
+    sha256 = "0pdfabb08b2plfns4ax3xjrwmq6z30qgrkgb1y0w0fjzvm28wz7g";
+  };
+
+  __structuredAttrs = true;
+
+  nativeBuildInputs = [
+    kubernetes-helm
+    yq
+  ];
+
+  helmArgs = [ ];
+  helmValues = { };
+
+  buildPhase = ''
+    runHook preBuild
+    yq --yaml-output '.helmValues' "$NIX_ATTRS_JSON_FILE" >values.yaml
+    helm template nats "$src" --output-dir . --values values.yaml "''${helmArgs[@]}"
+    runHook postBuild
+  '';
+
+  installPhase = ''
+    runHook preInstall
+    mkdir -p $out
+    cp -R ./nats/templates/* $out
+    runHook postInstall
+  '';
+
+  passthru.updateScript = [
+    "${lib.getExe nur.repos.josh.update-helm-chart-script}"
+    "https://nats-io.github.io/k8s/helm/charts"
+    "nats"
+    "./pkgs/nats-io/nats-charts.nix"
+  ];
+
+  meta = {
+    description = "NATS on Kubernetes with Helm Charts";
+    homepage = "https://github.com/nats-io/k8s/tree/main/helm/charts/nats";
+    license = lib.licenses.asl20;
+    platforms = lib.platforms.all;
+  };
+}
