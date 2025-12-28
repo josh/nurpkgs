a3ad41ad548ac694700a03f148b35ee7af4f9988
Joshua Peek <josh@users.noreply.github.com>
2025-12-28 14:41:32 -0800

    Add nack charts

---
+++ pkgs/nats-io/nack-charts.nix
@@ -0,0 +1,56 @@
+{
+  lib,
+  stdenvNoCC,
+  fetchzip,
+  kubernetes-helm,
+  yq,
+  nur,
+}:
+stdenvNoCC.mkDerivation {
+  pname = "nack-charts";
+  version = "0.31.1";
+
+  src = fetchzip {
+    url = "https://github.com/nats-io/k8s/releases/download/nack-0.31.1/nack-0.31.1.tgz";
+    sha256 = "04lmij4m78ljb9dl466dwvmxlqx1ldryp9hd1496n0akyymddvyx";
+  };
+
+  __structuredAttrs = true;
+
+  nativeBuildInputs = [
+    kubernetes-helm
+    yq
+  ];
+
+  helmArgs = [ ];
+  helmValues = { };
+
+  buildPhase = ''
+    runHook preBuild
+    yq --yaml-output '.helmValues' "$NIX_ATTRS_JSON_FILE" >values.yaml
+    helm template nack "$src" --output-dir . --values values.yaml "''${helmArgs[@]}"
+    runHook postBuild
+  '';
+
+  installPhase = ''
+    runHook preInstall
+    mkdir -p $out
+    cp -R ./nack/templates/* $out
+    runHook postInstall
+  '';
+
+  passthru.updateScript = [
+    "${lib.getExe nur.repos.josh.nixhelm-update}"
+    "--url"
+    "https://nats-io.github.io/k8s/helm/charts"
+    "--chart"
+    "nack"
+  ];
+
+  meta = {
+    description = "A Helm chart for NACK - NAts Controller for Kubernetes";
+    homepage = "https://github.com/nats-io/k8s/tree/main/helm/charts/nack";
+    license = lib.licenses.asl20;
+    platforms = lib.platforms.all;
+  };
+}
