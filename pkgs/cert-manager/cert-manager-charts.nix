8cfee7de60667d3ff49da6e2c1183d27444a2134
Joshua Peek <josh@users.noreply.github.com>
2025-12-28 12:07:56 -0800

    Add cert-manager-charts

---
+++ pkgs/cert-manager/cert-manager-charts.nix
@@ -0,0 +1,55 @@
+{
+  lib,
+  stdenvNoCC,
+  fetchzip,
+  kubernetes-helm,
+  yq,
+  nur,
+}:
+stdenvNoCC.mkDerivation {
+  pname = "cert-manager-charts";
+  version = "1.19.2";
+
+  src = fetchzip {
+    url = "https://quay.io/v2/jetstack/charts/cert-manager/blobs/sha256:87e2dafa946bd05c56be897b2fe2e171f2bbfad96c9d48409d4b1188d240af6f";
+    sha256 = "1yjvhigjx71ags1imcrkjz4i0i6zv5gpdcdn93hp6hjf5x52kxja";
+    extension = "tar.gz";
+  };
+
+  __structuredAttrs = true;
+
+  nativeBuildInputs = [
+    kubernetes-helm
+    yq
+  ];
+
+  helmArgs = [ ];
+  helmValues = { };
+
+  buildPhase = ''
+    runHook preBuild
+    yq --yaml-output '.helmValues' "$NIX_ATTRS_JSON_FILE" >values.yaml
+    helm template cert-manager "$src" --output-dir . --values values.yaml "''${helmArgs[@]}"
+    runHook postBuild
+  '';
+
+  installPhase = ''
+    runHook preInstall
+    mkdir -p $out
+    cp -R ./cert-manager/templates/* $out
+    runHook postInstall
+  '';
+
+  passthru.updateScript = [
+    "${lib.getExe nur.repos.josh.nixhelm-update}"
+    "--url"
+    "oci://quay.io/jetstack/charts/cert-manager"
+  ];
+
+  meta = {
+    description = "A Helm chart for cert-manager";
+    homepage = "https://cert-manager.io";
+    license = lib.licenses.bsd3;
+    platforms = lib.platforms.all;
+  };
+}
