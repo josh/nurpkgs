87e6d7fd92075fabc6d10c2c95838c6f7fa16d16
Joshua Peek <josh@users.noreply.github.com>
2026-01-10 13:14:17 -0800

    Add forgejo helm chart

---
+++ pkgs/forgejo/forgejo-chart.nix
@@ -0,0 +1,7 @@
+{ nur }:
+nur.repos.josh.fetchhelm {
+  url = "oci://code.forgejo.org/forgejo-helm/forgejo";
+  chart = "forgejo";
+  version = "15.0.4";
+  sha256 = "sha256-baYdDU71IodlK6EktpChlP9HDj4opWPYywbb9D5wYPo=";
+}
---
+++ pkgs/forgejo/forgejo-manifests.nix
@@ -0,0 +1,44 @@
+{
+  lib,
+  stdenvNoCC,
+  kubernetes-helm,
+  yq,
+  nur,
+}:
+stdenvNoCC.mkDerivation {
+  pname = "forgejo-chart";
+  inherit (nur.repos.josh.forgejo-chart) version;
+  src = nur.repos.josh.forgejo-chart;
+
+  __structuredAttrs = true;
+
+  nativeBuildInputs = [
+    kubernetes-helm
+    yq
+  ];
+
+  helmChartName = "forgejo";
+  helmArgs = [ ];
+  helmValues = { };
+
+  buildPhase = ''
+    runHook preBuild
+    yq --yaml-output '.helmValues' "$NIX_ATTRS_JSON_FILE" >values.yaml
+    helm template "$helmChartName" "$src" --output-dir . --values values.yaml "''${helmArgs[@]}"
+    runHook postBuild
+  '';
+
+  installPhase = ''
+    runHook preInstall
+    mkdir -p $out
+    cp -R ./"$helmChartName"/* $out
+    runHook postInstall
+  '';
+
+  meta = {
+    description = "Forgejo Helm chart";
+    homepage = "https://code.forgejo.org/forgejo-helm/forgejo-helm";
+    license = lib.licenses.mit;
+    platforms = lib.platforms.all;
+  };
+}
