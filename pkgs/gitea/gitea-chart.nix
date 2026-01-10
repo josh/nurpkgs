e3607b96a8dbcf14e6d678574e99d9a544393e77
Joshua Peek <josh@users.noreply.github.com>
2026-01-10 10:50:20 -0800

    Add gitea helm chart

---
+++ pkgs/gitea/gitea-chart.nix
@@ -0,0 +1,57 @@
+{
+  lib,
+  stdenvNoCC,
+  fetchzip,
+  kubernetes-helm,
+  yq,
+  nur,
+}:
+stdenvNoCC.mkDerivation {
+  pname = "gitea-chart";
+  version = "12.4.0";
+
+  src = fetchzip {
+    url = "https://dl.gitea.com/charts/gitea-12.4.0.tgz";
+    sha256 = "0y5wxixz20fanb0p8y6jkbjrgkz3k24z70hsjfrzppxgpwaw524c";
+  };
+
+  __structuredAttrs = true;
+
+  nativeBuildInputs = [
+    kubernetes-helm
+    yq
+  ];
+
+  helmChartName = "gitea";
+  helmArgs = [ ];
+  helmValues = { };
+
+  buildPhase = ''
+    runHook preBuild
+    yq --yaml-output '.helmValues' "$NIX_ATTRS_JSON_FILE" >values.yaml
+    helm template "$helmChartName" "$src" --output-dir . --values values.yaml "''${helmArgs[@]}"
+    runHook postBuild
+  '';
+
+  installPhase = ''
+    runHook preInstall
+    mkdir -p $out
+    cp -R ./"$helmChartName"/* $out
+    runHook postInstall
+  '';
+
+  passthru.updateScript = [
+    "${lib.getExe nur.repos.josh.nixhelm-update}"
+    "--url"
+    "https://dl.gitea.com/charts/"
+    "--chart"
+    "gitea"
+  ];
+
+  meta = {
+    description = "Gitea Helm chart for Kubernetes";
+    homepage = "https://gitea.com/gitea/helm-gitea";
+    license = lib.licenses.mit;
+    platforms = lib.platforms.all;
+  };
+}
