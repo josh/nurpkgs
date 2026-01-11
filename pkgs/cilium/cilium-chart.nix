abd4abd6be735588f2e83bed7df63d2bf504c542
Joshua Peek <josh@users.noreply.github.com>
2026-01-10 20:14:09 -0800

    Add cilium chart

---
+++ pkgs/cilium/cilium-chart.nix
@@ -0,0 +1,7 @@
+{ nur }:
+nur.repos.josh.fetchhelm {
+  url = "https://helm.cilium.io/";
+  chart = "cilium";
+  version = "1.18.5";
+  sha256 = "sha256-llAk2nlKO4VZ1weFR8gD4/CfdhWlNPNmSks+HYzjaB4=";
+}
