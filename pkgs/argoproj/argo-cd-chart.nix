b20a84aa2dd18fae38acd6cab413aa62af591b40
Joshua Peek <josh@users.noreply.github.com>
2026-01-10 20:11:50 -0800

    Add argo-cd helm chart

---
+++ pkgs/argoproj/argo-cd-chart.nix
@@ -0,0 +1,7 @@
+{ nur }:
+nur.repos.josh.fetchhelm {
+  url = "https://argoproj.github.io/argo-helm/";
+  chart = "argo-cd";
+  version = "9.2.4";
+  sha256 = "sha256-c8rtVYMnaaN4q6TlchzkD/hU7ZFxiM7WextnMpD3of4=";
+}
