5831099b1cc17ce128cfe4d8a9bbfdb00e86c666
Joshua Peek <josh@users.noreply.github.com>
2026-01-10 20:54:15 -0800

    Add harbor helm chart

---
+++ pkgs/goharbor/harbor-chart.nix
@@ -0,0 +1,7 @@
+{ nur }:
+nur.repos.josh.fetchhelm {
+  url = "https://helm.goharbor.io/";
+  chart = "harbor";
+  version = "1.18.1";
+  sha256 = "sha256-hAHyRjfDECw2XjM7Hrpbp4s2GaeSEz5IIjsA28pKmp8=";
+}
