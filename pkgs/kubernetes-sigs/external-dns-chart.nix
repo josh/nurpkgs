51b9ee04c10f42d98999d76f409861f41e9a2077
Joshua Peek <josh@users.noreply.github.com>
2026-01-10 20:16:40 -0800

    Add external-dns helm chart

---
+++ pkgs/kubernetes-sigs/external-dns-chart.nix
@@ -0,0 +1,7 @@
+{ nur }:
+nur.repos.josh.fetchhelm {
+  url = "https://kubernetes-sigs.github.io/external-dns/";
+  chart = "external-dns";
+  version = "1.20.0";
+  sha256 = "sha256-Y4Cpmxa4JvUIWFp9IkAI9ShdNihtK3wJDtRqwUJx+0o=";
+}
