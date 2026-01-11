a3150fbbc00edefb125143b2079b9021e78e184d
Joshua Peek <josh@users.noreply.github.com>
2026-01-10 20:36:04 -0800

    Add rook helm charts

---
+++ pkgs/ceph/rook-ceph-chart.nix
@@ -0,0 +1,7 @@
+{ nur }:
+nur.repos.josh.fetchhelm {
+  url = "https://charts.rook.io/release/";
+  chart = "rook-ceph";
+  version = "1.18.8";
+  sha256 = "sha256-CY0RBCLl9WX4KsI1lpLUKFHvltJs5dCm8Ssyv0ZSlmk=";
+}
---
+++ pkgs/ceph/rook-ceph-cluster-chart.nix
@@ -0,0 +1,7 @@
+{ nur }:
+nur.repos.josh.fetchhelm {
+  url = "https://charts.rook.io/release/";
+  chart = "rook-ceph-cluster";
+  version = "1.18.8";
+  sha256 = "sha256-oSA023QobXONRecG8/rBFKERX1n1iOH0tg08Uy8EUUc=";
+}
