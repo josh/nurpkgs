36a18d9ba9d283be2899e2c9225bad8612539306
Joshua Peek <josh@users.noreply.github.com>
2026-01-10 20:28:49 -0800

    Add actions-runner-controller charts

---
+++ pkgs/github/gha-runner-scale-set-chart.nix
@@ -0,0 +1,7 @@
+{ nur }:
+nur.repos.josh.fetchhelm {
+  url = "oci://ghcr.io/actions/actions-runner-controller-charts/gha-runner-scale-set";
+  chart = "gha-runner-scale-set";
+  version = "0.13.1";
+  sha256 = "sha256-E5k1OvJzE7lA8bdCJshL5nwzbOR6SeeGmW3iedh5/fQ=";
+}
---
+++ pkgs/github/gha-runner-scale-set-controller-chart.nix
@@ -0,0 +1,7 @@
+{ nur }:
+nur.repos.josh.fetchhelm {
+  url = "oci://ghcr.io/actions/actions-runner-controller-charts/gha-runner-scale-set-controller";
+  chart = "gha-runner-scale-set-controller";
+  version = "0.13.1";
+  sha256 = "sha256-qqM278S2PMBqGv2lbZanxkDn138/ZQA77JYVHufqxe8=";
+}
