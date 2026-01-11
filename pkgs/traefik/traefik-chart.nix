e58dc3a3b271645a793646ea45cd19787225109d
Joshua Peek <josh@users.noreply.github.com>
2026-01-10 20:21:47 -0800

    Add traefik helm chart

---
+++ pkgs/traefik/traefik-chart.nix
@@ -0,0 +1,7 @@
+{ nur }:
+nur.repos.josh.fetchhelm {
+  url = "https://traefik.github.io/charts/";
+  chart = "traefik";
+  version = "38.0.2";
+  sha256 = "sha256-gBI8pvmqqUgA6kzen6h5pkhjuCGvU/1rFFMRkvyN7qc=";
+}
