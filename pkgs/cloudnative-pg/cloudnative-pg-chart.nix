027e920922711dbbe39a152b4979a213a33da389
Joshua Peek <josh@users.noreply.github.com>
2026-01-10 22:49:41 -0800

    Add cnpg helm chart

---
+++ pkgs/cloudnative-pg/cloudnative-pg-chart.nix
@@ -0,0 +1,8 @@
+{ nur }:
+nur.repos.josh.fetchhelm {
+  pname = "cloudnative-pg-cluster";
+  url = "https://cloudnative-pg.github.io/charts";
+  chart = "cloudnative-pg";
+  version = "0.27.0";
+  sha256 = "sha256-gUIwNj/5VDO2ETWbwD3LlkVqNRoq2MpqLk8MUkWfafo=";
+}
---
+++ pkgs/cloudnative-pg/cloudnative-pg-cluster-chart.nix
@@ -0,0 +1,8 @@
+{ nur }:
+nur.repos.josh.fetchhelm {
+  pname = "cloudnative-pg-cluster";
+  url = "https://cloudnative-pg.github.io/charts";
+  chart = "cluster";
+  version = "0.5.0";
+  sha256 = "sha256-jWh+Thtgw81WFEgjDmP+22plI3v+sVDluNmIX0Uuxr0=";
+}
