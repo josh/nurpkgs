35f069d308697b1081c5238c6d4f87072e43648e
Joshua Peek <josh@users.noreply.github.com>
2026-01-10 20:55:14 -0800

    Add grafana helm chart

---
+++ pkgs/grafana/grafana-chart.nix
@@ -0,0 +1,7 @@
+{ nur }:
+nur.repos.josh.fetchhelm {
+  url = "https://grafana.github.io/helm-charts/";
+  chart = "grafana";
+  version = "10.5.5";
+  sha256 = "sha256-G06ipy5FWyW80KuBgg2U95VPi19cT3rO9zSuqxrQ4Ks=";
+}
