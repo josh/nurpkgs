b2264efc479cddfbdf9a9fda5bfb5ff625b30ab8
Joshua Peek <josh@users.noreply.github.com>
2026-01-10 20:39:40 -0800

    Add external-secrets chart

---
+++ pkgs/external-secrets/external-secrets-chart.nix
@@ -0,0 +1,7 @@
+{ nur }:
+nur.repos.josh.fetchhelm {
+  url = "https://charts.external-secrets.io/";
+  chart = "external-secrets";
+  version = "1.2.1";
+  sha256 = "sha256-7NMQ/2qMoxUhBhkTt+qb7zQJmBwV6KE7QfU2ACm6tRU=";
+}
