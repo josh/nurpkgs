e245224208d5d236bed94c6d053b54edfa487333
Joshua Peek <josh@users.noreply.github.com>
2026-01-10 20:58:03 -0800

    Add metallb chart

---
+++ pkgs/metallb/metallb-chart.nix
@@ -0,0 +1,7 @@
+{ nur }:
+nur.repos.josh.fetchhelm {
+  url = "https://metallb.github.io/metallb/";
+  chart = "metallb";
+  version = "0.15.3";
+  sha256 = "sha256-KWdVaF6CjFjeHQ6HT1WvkI9JnSurt9emLVCpkxma0fg=";
+}
