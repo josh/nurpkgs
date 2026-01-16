6a74f107948349d63182780dc4fe06f628ab2bae
Joshua Peek <josh@users.noreply.github.com>
2026-01-16 10:55:01 -0800

    Add headlamp chart

---
+++ pkgs/kubernetes-sigs/headlamp-chart.nix
@@ -0,0 +1,7 @@
+{ nur }:
+nur.repos.josh.fetchhelm {
+  url = "https://kubernetes-sigs.github.io/headlamp/";
+  chart = "headlamp";
+  version = "0.39.0";
+  sha256 = "sha256-AeWH4WSjrt/vhf81NreWG5msu2qiCt7IxLM+I47KyXE";
+}
