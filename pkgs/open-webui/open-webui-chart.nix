c9b8be920032551960a9fd64611422e063955c78
Joshua Peek <josh@users.noreply.github.com>
2026-01-16 15:33:49 -0800

    Add open-webui chart

---
+++ pkgs/open-webui/open-webui-chart.nix
@@ -0,0 +1,7 @@
+{ nur }:
+nur.repos.josh.fetchhelm {
+  url = "https://open-webui.github.io/helm-charts";
+  chart = "open-webui";
+  version = "10.2.1";
+  sha256 = "sha256-/7it0W1W3Bl684QGqQjFPMrnjkvw+NqGOaTEsMvzYHQ=";
+}
