5793b878ddb1fe3000602da1e5a3a4fe845d15b7
Joshua Peek <josh@users.noreply.github.com>
2026-01-16 22:31:01 -0800

    Add golink chart

---
+++ pkgs/tailscale/golink-chart.nix
@@ -0,0 +1,7 @@
+{ nur }:
+nur.repos.josh.fetchhelm {
+  url = "oci://ghcr.io/tiesmaster/golink-helm-chart/golink";
+  chart = "golink";
+  version = "0.5.0";
+  sha256 = "sha256-meeE0kwQL8UNGLnKMbr/r0x15LdCoZ2pLSDPldUdzaE=";
+}
