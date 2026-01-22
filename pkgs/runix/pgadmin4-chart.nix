bdb468aa362eecdd74b000c72582901a6432109b
Joshua Peek <josh@users.noreply.github.com>
2026-01-21 22:13:28 -0700

    Add pgadmin4 chart

---
+++ pkgs/runix/pgadmin4-chart.nix
@@ -0,0 +1,7 @@
+{ nur }:
+nur.repos.josh.fetchhelm {
+  url = "https://helm.runix.net";
+  chart = "pgadmin4";
+  version = "1.57.0";
+  sha256 = "sha256-BrTJG/moLpkzsKosgXX2G0NtkqeK5Z6ZYNiUmadcxkw=";
+}
