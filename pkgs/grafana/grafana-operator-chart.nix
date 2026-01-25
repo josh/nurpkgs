0b29b22d4c63a117bddaad9981ee6cbc5eb3741d
Joshua Peek <josh@users.noreply.github.com>
2026-01-25 12:14:56 -0700

    Add grafana-operator chart

---
+++ pkgs/grafana/grafana-operator-chart.nix
@@ -0,0 +1,7 @@
+{ nur }:
+nur.repos.josh.fetchhelm {
+  url = "https://grafana.github.io/helm-charts";
+  chart = "grafana-operator";
+  version = "5.21.4";
+  sha256 = "sha256-q6koOWzHOWN6JBtPqKBllbjak+WgscnXbfr30lLctfA=";
+}
