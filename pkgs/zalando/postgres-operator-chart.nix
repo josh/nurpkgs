06252bfb59aa8fbe90c6cd673ee3cebc67d64ab4
Joshua Peek <josh@users.noreply.github.com>
2026-01-25 15:40:21 -0700

    Add zalano postgres operator charts

---
+++ pkgs/zalando/postgres-operator-chart.nix
@@ -0,0 +1,8 @@
+{ nur }:
+nur.repos.josh.fetchhelm {
+  pname = "postgres-operator";
+  url = "https://opensource.zalando.com/postgres-operator/charts/postgres-operator";
+  chart = "postgres-operator";
+  version = "1.15.1";
+  sha256 = "sha256-bj4w/V3xzidIJrwmUwowcuwwEzYflzULCeHyNNJ9W/0=";
+}
---
+++ pkgs/zalando/postgres-operator-ui-chart.nix
@@ -0,0 +1,8 @@
+{ nur }:
+nur.repos.josh.fetchhelm {
+  pname = "postgres-operator-ui";
+  url = "https://opensource.zalando.com/postgres-operator/charts/postgres-operator-ui";
+  chart = "postgres-operator-ui";
+  version = "1.15.1";
+  sha256 = "sha256-/GW07S8H8QbKcVdybrFZNyGdJazZfKlXRj+3eFgi2uU=";
+}
