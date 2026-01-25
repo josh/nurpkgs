21dea4ef0f663363e352844e9c0efcbd9c3cdc6f
Joshua Peek <josh@users.noreply.github.com>
2026-01-25 10:57:40 -0700

    Add terraform-provider-unifi backports

---
+++ pkgs/ubiquiti-community/terraform-provider-unifi-old.nix
@@ -0,0 +1,11 @@
+{ terraform-providers }:
+terraform-providers.mkProvider {
+  owner = "josh";
+  repo = "terraform-provider-unifi";
+  rev = "v0.41.3";
+  hash = "sha256-IqsQRVgAB1PUqYJ0JPig6oq7FKJuS/a2NCgTCxX9+cg=";
+  vendorHash = "sha256-iqwdSzyVZE5CitzXc8rWn2zO3v+VvuDNeS0ll0V6MtU=";
+  provider-source-address = "registry.terraform.io/ubiquiti-community/unifi";
+  homepage = "https://github.com/ubiquiti-community/terraform-provider-unifi";
+  spdx = "MPL-2.0";
+}
---
+++ pkgs/ubiquiti-community/terraform-provider-unifi.nix
@@ -0,0 +1,26 @@
+{ terraform-providers, nix-update-script }:
+let
+  pkg = terraform-providers.mkProvider {
+    owner = "josh";
+    repo = "terraform-provider-unifi";
+    rev = "v0.41.3";
+    hash = "sha256-IqsQRVgAB1PUqYJ0JPig6oq7FKJuS/a2NCgTCxX9+cg=";
+    vendorHash = "sha256-iqwdSzyVZE5CitzXc8rWn2zO3v+VvuDNeS0ll0V6MtU=";
+    provider-source-address = "registry.terraform.io/ubiquiti-community/unifi";
+    homepage = "https://github.com/ubiquiti-community/terraform-provider-unifi";
+    spdx = "MPL-2.0";
+  };
+in
+pkg.overrideAttrs (
+  _finalAttrs: previousAttrs: {
+    passthru = previousAttrs.passthru // {
+      updateScript = nix-update-script {
+        extraArgs = [
+          "--version=stable"
+          "--override-filename"
+          "pkgs/josh/terraform-provider-unifi.nix"
+        ];
+      };
+    };
+  }
+)
