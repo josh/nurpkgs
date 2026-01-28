78d74db166e5baf00c8f806ea20df3af87c51874
Joshua Peek <josh@users.noreply.github.com>
2026-01-28 11:26:03 -0800

    Remove deprecated kubernetes dashboard

--- pkgs/kubernetes/kubernetes-dashboard-chart.nix
+++
@@ -1,7 +0,0 @@
-{ nur }:
-nur.repos.josh.fetchhelm {
-  url = "https://kubernetes.github.io/dashboard";
-  chart = "kubernetes-dashboard";
-  version = "7.14.0";
-  sha256 = "sha256-n0HvDe1+pS9Zu4JqP9PwWkubAvp9F8KGaB0tPaLShHA=";
-}
