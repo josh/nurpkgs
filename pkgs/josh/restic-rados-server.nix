f10194086610bcde18b6c7347d81da3090936baf
Joshua Peek <josh@users.noreply.github.com>
2026-01-28 15:13:38 -0800

    Rename restic-rados-server

--- pkgs/josh/restic-rados-server.nix
+++ pkgs/josh/restic-rados-server.nix
@@ -7,14 +7,14 @@
   restic,
 }:
 buildGoModule (finalAttrs: {
-  pname = "restic-ceph-server";
+  pname = "restic-rados-server";
   version = "0-unstable-2026-01-28";
 
   src = fetchFromGitHub {
     owner = "josh";
-    repo = "restic-ceph-server";
-    rev = "b368cb02666c495603964c721611c2837f7c0fc4";
-    hash = "sha256-nvvc81Buz5qeVg8T82/DV/1lXO2uPE4iiiP7gzwVgcE=";
+    repo = "restic-rados-server";
+    rev = "116fa2c63379ae0693ca49263e77f36ef3215fc4";
+    hash = "sha256-0co5PlX9XFe4v9MCW/37kFy8Z6o8gao4l85VY37boUY=";
   };
 
   vendorHash = "sha256-YJQIf2fXcDnwHXwVpzS/k0xXbKme/Hk60R1h6gSNTSc=";
@@ -42,9 +42,9 @@ buildGoModule (finalAttrs: {
 
   meta = {
     description = "A restic repository backend that stores data in raw Ceph RADOS";
-    homepage = "https://github.com/josh/restic-ceph-server";
+    homepage = "https://github.com/josh/restic-rados-server";
     license = lib.licenses.mit;
     inherit (ceph.meta) platforms;
-    mainProgram = "restic-ceph-server";
+    mainProgram = "restic-rados-server";
   };
 })
