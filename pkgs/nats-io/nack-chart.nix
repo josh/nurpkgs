ba2a084f21f3088f41476996ba58ea5af57745f6
Joshua Peek <josh@users.noreply.github.com>
2026-02-01 15:00:18 -0800

    Disable nack chart updates

--- pkgs/nats-io/nack-chart.nix
+++ pkgs/nats-io/nack-chart.nix
@@ -1,7 +1,12 @@
 { nur }:
-nur.repos.josh.fetchhelm {
-  url = "https://nats-io.github.io/k8s/helm/charts";
-  chart = "nack";
-  version = "0.31.1";
-  sha256 = "sha256-3e/WqvdTAWsSCQ2m63OjoWPa6+bNGEJbWpKiU4mMlRI=";
-}
+let
+  chart = nur.repos.josh.fetchhelm {
+    url = "https://nats-io.github.io/k8s/helm/charts";
+    chart = "nack";
+    version = "0.31.1";
+    sha256 = "sha256-3e/WqvdTAWsSCQ2m63OjoWPa6+bNGEJbWpKiU4mMlRI=";
+  };
+in
+chart.overrideAttrs (oldAttrs: {
+  passthru = builtins.removeAttrs oldAttrs.passthru [ "updateScript" ];
+})
