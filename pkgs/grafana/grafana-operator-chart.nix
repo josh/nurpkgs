ea8f52e7570a4b240aa3de677a448af97c2a35e6
Joshua Peek <josh@users.noreply.github.com>
2026-02-03 14:25:14 -0800

    Use oci image for helm chart

--- pkgs/grafana/grafana-operator-chart.nix
+++ pkgs/grafana/grafana-operator-chart.nix
@@ -1,6 +1,6 @@
 { nur }:
 nur.repos.josh.fetchhelm {
-  url = "https://grafana.github.io/helm-charts";
+  url = "oci://ghcr.io/grafana/helm-charts/grafana-operator";
   chart = "grafana-operator";
   version = "5.21.4";
   sha256 = "sha256-q6koOWzHOWN6JBtPqKBllbjak+WgscnXbfr30lLctfA=";
