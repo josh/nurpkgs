fa13bb7667932c47bd7d6d78c7d90c96cb2d8b59
Joshua Peek <josh@users.noreply.github.com>
2026-02-07 15:31:29 -0800

    Add tuppr chart

---
+++ pkgs/home-operations/tuppr-chart.nix
@@ -0,0 +1,7 @@
+{ nur }:
+nur.repos.josh.fetchhelm {
+  url = "oci://ghcr.io/home-operations/charts/tuppr";
+  chart = "tuppr";
+  version = "0.0.52";
+  sha256 = "sha256-6UCDM8IqONhNB3BV6O8j4DSKNh/l3OCTe2C4yYwRZA0=";
+}
