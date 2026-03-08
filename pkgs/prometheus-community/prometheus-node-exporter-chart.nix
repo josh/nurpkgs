f9c75127417d7085901ff775a00ea0c118312318
Joshua Peek <josh@users.noreply.github.com>
2026-03-08 16:02:38 -0700

    prometheus-node-exporter-chart: init at 4.52.0

---
+++ pkgs/prometheus-community/prometheus-node-exporter-chart.nix
@@ -0,0 +1,7 @@
+{ nur }:
+nur.repos.josh.fetchhelm {
+  url = "https://prometheus-community.github.io/helm-charts";
+  chart = "prometheus-node-exporter";
+  version = "4.52.0";
+  sha256 = "sha256-9jTRJB6DPWt+/sMjJHxvmhkWSRcZlDY0mJFDqiCdbbQ=";
+}
