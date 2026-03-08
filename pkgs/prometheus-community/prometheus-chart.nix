6404faf8f4cd81a856e7161ac79b9fb83502b142
Joshua Peek <josh@users.noreply.github.com>
2026-03-08 16:09:32 -0700

    prometheus-chart: init at 28.13.0

---
+++ pkgs/prometheus-community/prometheus-chart.nix
@@ -0,0 +1,7 @@
+{ nur }:
+nur.repos.josh.fetchhelm {
+  url = "https://prometheus-community.github.io/helm-charts";
+  chart = "prometheus";
+  version = "28.13.0";
+  sha256 = "sha256-NAEfT/KXWS6ib1/GIr0NHjxmcbcvdfr1TBFMDtDIARY=";
+}
