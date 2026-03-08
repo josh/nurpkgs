fae05a2a6db46080ff9037d67cff4989bbc473fd
Joshua Peek <josh@users.noreply.github.com>
2026-03-08 16:06:33 -0700

    kube-prometheus-stack-chart: init at 82.10.1

---
+++ pkgs/prometheus-community/kube-prometheus-stack-chart.nix
@@ -0,0 +1,7 @@
+{ nur }:
+nur.repos.josh.fetchhelm {
+  url = "https://prometheus-community.github.io/helm-charts";
+  chart = "kube-prometheus-stack";
+  version = "82.10.1";
+  sha256 = "sha256-peKXTkzoKC0FThFMFAapBHaFBbb8XrGAutzTMUWXgFY=";
+}
