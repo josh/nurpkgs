b64904d5eb14dc4c97bff41c8ff1bae9f3c4edb5
Joshua Peek <josh@users.noreply.github.com>
2026-03-08 16:04:48 -0700

    kube-state-metrics-chart: init at 7.2.0

---
+++ pkgs/prometheus-community/kube-state-metrics-chart.nix
@@ -0,0 +1,7 @@
+{ nur }:
+nur.repos.josh.fetchhelm {
+  url = "https://prometheus-community.github.io/helm-charts";
+  chart = "kube-state-metrics";
+  version = "7.2.0";
+  sha256 = "sha256-U8GbnlvxG9alzEUyeYEIB5qgq1Dxn20rebptokUdyF0=";
+}
