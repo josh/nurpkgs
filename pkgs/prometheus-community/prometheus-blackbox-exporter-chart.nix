ac03d27b033f8425084cef25890c49d1b90bee4e
Joshua Peek <josh@users.noreply.github.com>
2026-03-08 16:08:05 -0700

    prometheus-blackbox-exporter-chart: init at 11.8.0

---
+++ pkgs/prometheus-community/prometheus-blackbox-exporter-chart.nix
@@ -0,0 +1,7 @@
+{ nur }:
+nur.repos.josh.fetchhelm {
+  url = "https://prometheus-community.github.io/helm-charts";
+  chart = "prometheus-blackbox-exporter";
+  version = "11.8.0";
+  sha256 = "sha256-mNpGevgIqA0F8MJ8IVzqDCLSJ6OGcUlJLC5Fft2hErE=";
+}
