94c5fa462a06cc7ffeed9082472aac65ed1336d9
Joshua Peek <josh@users.noreply.github.com>
2026-03-27 13:52:29 -0700

    Add Bitwarden self-host and sm-operator helm charts

---
+++ pkgs/bitwarden/bitwarden-self-host-chart.nix
@@ -0,0 +1,7 @@
+{ nur }:
+nur.repos.josh.fetchhelm {
+  url = "https://charts.bitwarden.com/";
+  chart = "self-host";
+  version = "1.1.9";
+  sha256 = "sha256-jYyjWTA4AIkFb9VnxXFgSLmlXl/ipwrxREaiXWF0tjk=";
+}
---
+++ pkgs/bitwarden/bitwarden-sm-operator-chart.nix
@@ -0,0 +1,7 @@
+{ nur }:
+nur.repos.josh.fetchhelm {
+  url = "https://charts.bitwarden.com/";
+  chart = "sm-operator";
+  version = "2.0.1";
+  sha256 = "sha256-qjxX4ke/WqaaG6ag5+HL50tyR3a0l8PGHOvAjknrH6U=";
+}
