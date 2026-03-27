2710bb645517fe50ae54707b90b827453fef94f7
Joshua Peek <josh@users.noreply.github.com>
2026-03-27 15:38:19 -0700

    Add vaultwarden helm chart

---
+++ pkgs/guerzon/vaultwarden-chart.nix
@@ -0,0 +1,7 @@
+{ nur }:
+nur.repos.josh.fetchhelm {
+  url = "https://guerzon.github.io/vaultwarden";
+  chart = "vaultwarden";
+  version = "0.35.1";
+  sha256 = "sha256-UJybkaqLJK9omp1L2PG6MyV3wpqp86rl1Ky2nBnzM1U=";
+}
