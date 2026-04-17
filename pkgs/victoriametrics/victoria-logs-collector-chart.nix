a66bcbf81b5f044ec769116620de7ff0c921aa13
Joshua Peek <josh@users.noreply.github.com>
2026-04-17 13:35:40 -0700

    Add render and image tag validation tests to fetchhelm

--- pkgs/victoriametrics/victoria-logs-collector-chart.nix
+++ pkgs/victoriametrics/victoria-logs-collector-chart.nix
@@ -4,4 +4,9 @@ nur.repos.josh.fetchhelm {
   chart = "victoria-logs-collector";
   version = "0.3.0";
   sha256 = "sha256-kepiKw7pke754Y2B85rG2nVNE2dugw8Of31NfP4JnWE=";
+  helmTestValues = {
+    remoteWrite = [
+      { url = "http://localhost:9428"; }
+    ];
+  };
 }
