4fa3ef687448d59b44115d257b852c0d6f6fe366
Joshua Peek <josh@users.noreply.github.com>
2026-04-25 08:12:31 -0700

    Remove archived cased packages

--- pkgs/cased/cased-cd-enterprise-chart.nix
+++
@@ -1,7 +0,0 @@
-{ nur }:
-nur.repos.josh.fetchhelm {
-  url = "https://cased.github.io/cased-cd-enterprise";
-  chart = "cased-cd-enterprise";
-  version = "0.2.25";
-  sha256 = "sha256-ZcckEAZKOeA0eHY375oG5Zwu93uJgfCbFPzueELFQXY=";
-}
