9617cc4f02d02033062450d91eda07505b2d3402
Joshua Peek <josh@users.noreply.github.com>
2026-05-15 08:13:15 -0700

    sops-secrets-operator: pin --kube-version to 1.36.0 for templating

--- pkgs/isindir/sops-secrets-operator-chart.nix
+++ pkgs/isindir/sops-secrets-operator-chart.nix
@@ -4,4 +4,8 @@ nur.repos.josh.fetchhelm {
   chart = "sops-secrets-operator";
   version = "0.27.0";
   sha256 = "sha256-pfUhIXEhh8oGKONq8a3cTAdxz6QSUTgOtfAFnwPsqRg=";
+  helmTestArgs = [
+    "--kube-version"
+    "1.36.0"
+  ];
 }
