d00e80873e916e9db24b796efc3804845bb791a4
Joshua Peek <josh@users.noreply.github.com>
2026-05-18 11:57:36 -0700

    jmap2nats: init at 0.0.3

---
+++ pkgs/josh/jmap2nats.nix
@@ -0,0 +1,34 @@
+{
+  lib,
+  buildGoModule,
+  fetchFromGitHub,
+  nix-update-script,
+}:
+buildGoModule (finalAttrs: {
+  pname = "jmap2nats";
+  version = "0.0.3";
+
+  src = fetchFromGitHub {
+    owner = "josh";
+    repo = "jmap2nats";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-cxml1lWbub2lT/BujKBjXcA3GFjEAQqrbC74cYPZWI4=";
+  };
+
+  vendorHash = "sha256-osoKMrdwMTlgOXzTSeDeop7l8UiZqAAYYL5cpCQ1gEo=";
+
+  ldflags = [
+    "-s"
+    "-w"
+  ];
+
+  passthru.updateScript = nix-update-script { extraArgs = [ "--version=stable" ]; };
+
+  meta = {
+    description = "Bridge JMAP email push events to NATS JetStream";
+    homepage = "https://github.com/josh/jmap2nats";
+    license = lib.licenses.mit;
+    platforms = lib.platforms.all;
+    mainProgram = "jmap2nats";
+  };
+})
