393ea459bb89334f6f547cab22f7682a7a77ccf3
Joshua Peek <josh@users.noreply.github.com>
2026-07-30 14:31:00 -0700

    internal: order arguments per convention

--- internal/fetchhelm.nix
+++ internal/fetchhelm.nix
@@ -5,11 +5,11 @@
   kubernetes-helm,
 }:
 args@{
-  pname ? "${chart}-chart",
   url,
   chart,
   version,
   hash,
+  pname ? "${chart}-chart",
   helmTestValues ? { },
   helmTestArgs ? [ ],
   meta ? { },
