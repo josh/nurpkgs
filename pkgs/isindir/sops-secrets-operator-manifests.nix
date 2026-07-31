956e9307be9d26376c4d6d4dd03751f39aae209d
Joshua Peek <josh@users.noreply.github.com>
2026-07-31 14:40:12 -0700

    manifests: copy rendered output including dotfiles

--- pkgs/isindir/sops-secrets-operator-manifests.nix
+++ pkgs/isindir/sops-secrets-operator-manifests.nix
@@ -39,7 +39,7 @@ stdenvNoCC.mkDerivation (finalAttrs: {
   installPhase = ''
     runHook preInstall
     mkdir -p $out
-    cp -R ./"$helmChartName"/* $out
+    cp -R ./"$helmChartName"/. $out/
     runHook postInstall
   '';
 
